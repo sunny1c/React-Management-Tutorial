737ea402242779e8a5e44a107216512433938cc1
sunny1c <choihw522@gmail.com>
2021-03-31 21:25:07 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+import React, {Component} from 'react';
+
+const customer = {
+  'name' : '최해원',
+  'birthday' : '910522',
+  'gender' : '여자',
+  'job' : '직장인'
+}
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
